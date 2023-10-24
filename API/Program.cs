daa4032d5754d4668237d40427cdaab5a9fdfe66
Bùi Tiến Dũng <dungbuivm@gmail.com>
2023-10-24 09:39:52 +0700

    Adding CORS || Http requests in Angular || Display the Fetched User

--- API/Program.cs
+++ API/Program.cs
@@ -12,10 +12,14 @@ builder.Services.AddDbContext<DataContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+// fix lỗi CORS
+builder.Services.AddCors();
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
 app.UseHttpsRedirection();
 
